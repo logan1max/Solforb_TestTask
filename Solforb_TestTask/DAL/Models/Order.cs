462af7253a72ec31ca1faa1eff49347bee1a171d
Максим Колосов <logan1.max@gmail.com>
2022-11-14 16:28:10 +0100

    Добавьте файлы проекта.

---
+++ Solforb_TestTask/DAL/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Solforb_TestTask.DAL.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public DateTime Date { get; set; }
+        public int ProviderId { get; set; }
+    }
+}
---
+++ Solforb_TestTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Solforb_TestTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
